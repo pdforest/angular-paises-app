85c6f1bd61bdd8a82a37048322192cab605baab2
pdforest <pdforest@gmail.com>
2023-06-21 17:33:11 -0300

    add useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,38 +8,15 @@ import { VerPaisComponent } from "./pais/pages/ver-pais/ver-pais.component";
 
 
 const routes: Routes = [
-    {
-        path: '',
-        component: PorPaisComponent,
-        pathMatch: 'full'
-    },
-    {
-        path: 'region',
-        component: PorRegionComponent
-    },
-    {
-        path: 'capital',
-        component: PorCapitalComponent
-    },
-    {
-        path: 'pais/:id',
-        component: VerPaisComponent
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    }
+    { path: '',         component: PorPaisComponent, pathMatch: 'full' },
+    { path: 'region',   component: PorRegionComponent },
+    { path: 'capital',  component: PorCapitalComponent },
+    { path: 'pais/:id', component: VerPaisComponent },
+    { path: '**', redirectTo: '' }
 ]
 
-
-
 @NgModule({
-    imports: [
-        RouterModule.forRoot( routes )
-    ],
-    exports: [
-        RouterModule
-    ]
+    imports: [RouterModule.forRoot( routes, {useHash: true} )],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {}
-
